bfd7a979fd47d19009c7bd5d739d112b6e957155
sakib <bhatsakibali@gmail.com>
2023-03-31 12:52:11 +0530

    installedPX Tag

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,11 @@
 
 
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-QESVF5GBK2CM-2");
+
   
 function allowlogin(usermail)
 {
